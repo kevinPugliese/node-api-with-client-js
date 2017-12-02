56a79fbcb02ba516cbcfd4452a7af88810c9581c
Kevin Pugliese <kevin.pugliese@clickbus.com>
2017-12-02 17:15:33 -0200

    add variables gulpfile

--- buscape-project/client/gulpfile.js
+++ buscape-project/client/gulpfile.js
@@ -1,45 +1,73 @@
-'use strict';
+// *************************************
+//
+//   Gulpfile
+//
+// *************************************
+//
+// Available tasks:
+//   `gulp`
+//   `gulp clean`
+//   `gulp copy`
+//   `gulp build-img`
+//   `gulp sass`
+//   `gulp usemin`
+//
+// *************************************
 
-var gulp = require('gulp')
-    ,imagemin = require('gulp-imagemin')
-    ,clean = require('gulp-clean')
-    ,uglify = require('gulp-uglify')
-    ,usemin = require('gulp-usemin')
-    ,cssmin = require('gulp-cssmin')
-    ,sass = require('gulp-sass');
+var gulp        = require('gulp')
+    ,imagemin   = require('gulp-imagemin')
+    ,clean      = require('gulp-clean')
+    ,uglify     = require('gulp-uglify')
+    ,usemin     = require('gulp-usemin')
+    ,cssmin     = require('gulp-cssmin')
+    ,sass       = require('gulp-sass');
+
+
+var options     = {
+
+    dist        : 'dist',
+    imgs        : 'dist/assets/img/**/*',
+    imgsDist    : 'dist/assets/img',
+    src         : 'src/**/*',
+    bootstrap   : 'dist/assets/scss/bootstrap.scss',
+    scss        : 'dist/assets/scss/',
+    html        : 'dist/**/*.html'
+};
+
+console.log(options.imgs);
 
 gulp.task('default', ['copy'], function() {
     gulp.start('build-img', 'usemin');
 });
 
 gulp.task('clean', function() {
-    return gulp.src('dist')
+    return gulp.src(options.dist)
         .pipe(clean());
 });
 
 gulp.task('copy', ['clean'], function() {
-    return gulp.src('src/**/*')
-        .pipe(gulp.dest('dist'));
+    return gulp.src(options.src)
+        .pipe(gulp.dest(options.dist));
 });
 
 gulp.task('build-img', function() {
 
-    return gulp.src('dist/assets/img/**/*')
+    return gulp.src(options.imgs)
         .pipe(imagemin())
-        .pipe(gulp.dest('dist/assets/img'));
+        .pipe(gulp.dest(options.imgsDist));
 });
 
 gulp.task('sass', function () {
-    return gulp.src('dist/assets/scss/bootstrap.scss')
+    return gulp.src(options.bootstrap)
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('dist/assets/scss/'));
+        .pipe(gulp.dest(options.scss));
 });
 
 gulp.task('usemin', ['sass'],function() {
-    return gulp.src('dist/**/*.html')
+    return gulp.src(options.html)
         .pipe(usemin({
             js: [uglify],
             css: [cssmin]
         }))
-        .pipe(gulp.dest('dist/'));
+        .pipe(gulp.dest(options.dist));
 });

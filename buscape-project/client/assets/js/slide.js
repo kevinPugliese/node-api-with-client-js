b1e9abc77dcf5790fb5e10916896756f84b0f551
Kevin Pugliese <kevin.pugliese@clickbus.com>
2017-12-10 12:59:37 -0200

    Slide js

--- buscape-project/client/assets/js/slide.js
+++ buscape-project/client/assets/js/slide.js
@@ -1,3 +1,42 @@
-/**
- * Created by kevin.pugliese on 09/12/17.
- */
+Buscape = typeof Buscape === 'undefined' ? {} : Buscape;
+Buscape.Slide = function () {
+
+    var $_container = $('.section-content');
+    var _slide = '.slide-img';
+    var url_img_error = 'http://4.bp.blogspot.com/-W1s91N1YUQc/URauAIgv_-I/AAAAAAAAAF8/BC9sWDKQQbQ/s210/uiukjnggfgf.jpg';
+
+    /**
+     * Verify url is 404
+     *
+     * @param imageUrl
+     * @returns {*}
+     * @private
+     */
+    var _checkImg = function(imageUrl) {
+        var img = new Image();
+        img.src = imageUrl;
+
+        if (!img.height > 0) {
+            return url_img_error;
+        }
+        return imageUrl;
+    };
+
+    /**
+     * Show Img slide clicked
+     *
+     * @private
+     */
+    var _slideImg = function () {
+        var src = _checkImg($(this).attr('src'));
+        var img = $(this).closest('div').children('img');
+
+        img.attr('src', src);
+    };
+
+    return {
+        init: function () {
+            $_container.on('click', _slide, _slideImg);
+        }
+    }
+};

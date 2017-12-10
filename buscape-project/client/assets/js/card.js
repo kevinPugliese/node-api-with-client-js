d5b299cae0ded0404466c2ff26a325a9f87468f6
Kevin Pugliese <kevin.pugliese@clickbus.com>
2017-12-10 12:58:17 -0200

    Card js

--- buscape-project/client/assets/js/card.js
+++ buscape-project/client/assets/js/card.js
@@ -1,11 +1,22 @@
 Buscape = typeof Buscape === 'undefined' ? {} : Buscape;
-Buscape.Home = function () {
+Buscape.Card = function () {
 
+    var _api = Buscape.API();
+    var _templateHtml = Buscape.Template();
 
+    /**
+     * Get products from API and render template Card
+     * @private
+     */
+    var _showCards = function () {
+        _api.getProducts(function (products) {
+            _templateHtml.getTemplate('#template-card-product', products[0], '#target');
+        });
+    };
 
     return {
         init: function () {
-
+            _showCards();
         }
     }
 };

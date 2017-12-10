c80a9078e8327128ff7bf632a91d12c77a4a8668
Kevin Pugliese <kevin.pugliese@clickbus.com>
2017-12-10 12:58:51 -0200

    Menu js

--- buscape-project/client/assets/js/menu.js
+++ buscape-project/client/assets/js/menu.js
@@ -1,3 +1,21 @@
-/**
- * Created by kevin.pugliese on 09/12/17.
- */
+Buscape = typeof Buscape === 'undefined' ? {} : Buscape;
+Buscape.Menu = function () {
+
+    var $_menu_drop = $('.menu-drop-down');
+    var $_open_menu = 'open-menu';
+    var $_menu = $('.menu-drop');
+
+    /**
+     * Show Menu
+     * @private
+     */
+    var _menu = function () {
+        $_menu_drop.toggle($_open_menu);
+    };
+
+    return {
+        init: function () {
+            $_menu.on('click', _menu);
+        }
+    }
+};
